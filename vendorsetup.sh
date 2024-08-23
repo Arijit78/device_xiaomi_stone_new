dae0943655ef1a4900b5a9448297d3b5c701a2c2
Arijit78 <sahaarijit2007@gmail.com>
2024-08-23 16:49:28 +0000

    stone: Track kernel from los

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -11,7 +11,7 @@ git clone https://github.com/Arijit78/vendor_dolby_oplus.git -b udc vendor/dolby
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-21 hardware/xiaomi
 
 # Clone Kernel Source
-git clone https://github.com/Stone-Trees/kernel_xiaomi_sm6375.git -b 14 kernel/xiaomi/sm6375
+git clone https://github.com/crdroidandroid/android_kernel_qcom_sm8350.git -b 14.0 kernel/xiaomi/sm6375
 
 # Clone v4a
 git clone --depth=1 https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git -b v4a packages/apps/ViPER4AndroidFX
