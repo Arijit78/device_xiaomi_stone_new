8e8c36994852c6d648c4788669ddca11cbb65880
kamikaonashi <thorschi93@gmx.at>
2024-09-13 17:17:22 +0200

    stone: overlay: show correct current battery mAh
    based on:
    https://review.lineageos.org/c/LineageOS/android_device_xiaomi_onclite/+/402437
    
    set to 1, since our device reports the correct info by default

--- device.mk
+++ device.mk
@@ -384,6 +384,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 # Overlays RRO
 PRODUCT_PACKAGES += \
+    BatteryOverlayStone \
     CarrierConfigOverlayStone \
     WifiOverlayStone \
     FrameworksResOverlayStone \
