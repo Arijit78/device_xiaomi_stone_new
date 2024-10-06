78bf81c3839609f9147382d1393f5abaefc6357f
Arijit78 <sahaarijit2007@gmail.com>
2024-10-06 05:01:46 +0000

    stone: Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -11,11 +11,11 @@ git clone https://github.com/Arijit78/vendor_dolby_oplus.git -b udc vendor/dolby
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-21 hardware/xiaomi
 
 # Clone Kernel Source
-git clone https://github.com/crdroidandroid/android_kernel_qcom_sm8350.git -b 14.0 kernel/xiaomi/sm6375
+git clone --depth=1 https://github.com/LineageOS/android_kernel_qcom_sm8350.git kernel/xiaomi/sm6375
 
 # Clone v4a
 git clone --depth=1 https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git -b v4a packages/apps/ViPER4AndroidFX
 
 # Clone Qcom power
 rm -rf vendor/qcom/opensource/power
-git clone https://github.com/kamikaonashi/android_vendor_qcom_opensource_power.git -b lineage-21.0 vendor/qcom/opensource/power
+git clone https://github.com/Arijit78/android_vendor_qcom_opensource_power.git -b lineage-21.0 vendor/qcom/opensource/power
